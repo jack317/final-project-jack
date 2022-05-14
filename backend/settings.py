c9f42c794577e76bdb4d068b0f939324c3c678da
Jack <jack@Jacks-MacBook-Pro.local>
2022-05-13 21:48:41 -0700

    - changes

--- backend/settings.py
+++ backend/settings.py
@@ -143,11 +143,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 #location where django collect all static files
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # # location where you will store your static files
-# STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')]
 
 
 # Default primary key field type
