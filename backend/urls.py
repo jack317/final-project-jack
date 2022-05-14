94cb32f46197cab79ef8477334aef90a16bb4009
Jack <jack@Jacks-MacBook-Pro.local>
2022-05-13 23:21:11 -0700

    - add home render

--- backend/urls.py
+++ backend/urls.py
@@ -23,6 +23,7 @@ router.register('Paragraph', views.ParagraphView)
 router.register('Saved', views.SavedView)
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
